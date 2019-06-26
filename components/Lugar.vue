f2736acd6f6c5a1b2a8f33131a9ba179bb893cec
Juan <jn90pp@gmail.com>
2019-06-26 16:32:11 -0500

    first commit of the project

---
+++ components/Lugar.vue
@@ -0,0 +1,27 @@
+<template>
+  <section id="cuadroprincipal">
+    <article class="container">
+      <div class="cont-photo row">
+        <div class="col-md-8">
+          <figure>
+            <img
+              src="~/assets/canchas/lospinos.jpg"
+              class="figure-img img-fluid rounded"
+              alt="Los Pinos"
+            />
+          </figure>
+        </div>
+        <div class="col-md-4">
+          <ul class="list-group">
+            <li class="list-group-item active">Los Pinos</li>
+            <li class="list-group-item text-left">
+              Martes/Jueves/Domingo 4:15pm - 7:00pm
+            </li>
+            <li class="list-group-item text-left">Sabado 7:30pm - 10:00pm</li>
+            <li class="list-group-item text-left">Nivel Barrio</li>
+          </ul>
+        </div>
+      </div>
+    </article>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">voleicito</h1>
+      <h2 class="subtitle">Encuentra donde jugar voley</h2>
+      <Lugar />
+    </div>
+  </div>
+</template>
+
+<script>
+import Lugar from '~/components/Lugar.vue'
+
+export default {
+  components: {
+    Lugar
+  }
+}
+</script>
