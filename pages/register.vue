6262598008756b28e6b64271130bc81db08fa4e3
Juan <jn90pp@gmail.com>
2020-10-31 18:14:02 -0500

    updating packages and vetur configuration

--- pages/register.vue
+++ pages/register.vue
@@ -2,9 +2,10 @@
   <div class="container">
     <div class="py-5 text-center">
       <h2>Registro de Lugar</h2>
-      <p
-        class="lead"
-      >Registra los datos del lugar donde se juega voley, indicar los datos exactos para evitar confusiones.</p>
+      <p class="lead">
+        Registra los datos del lugar donde se juega voley, indicar los datos
+        exactos para evitar confusiones :).
+      </p>
     </div>
     <div class="row">
       <div class="col-md-12">
@@ -12,11 +13,21 @@
           <form>
             <div class="form-group">
               <label for="txtNombre">Como se llama?</label>
-              <input id="txtNombre" type="text" class="form-control" placeholder="Lugar" />
+              <input
+                id="txtNombre"
+                type="text"
+                class="form-control"
+                placeholder="Lugar"
+              />
             </div>
             <div class="form-group">
               <label for="txtDonde">Donde queda?</label>
-              <input id="txtDonde" type="text" class="form-control" placeholder="Dirección" />
+              <input
+                id="txtDonde"
+                type="text"
+                class="form-control"
+                placeholder="Dirección"
+              />
             </div>
             <div class="form-group">
               <label for="sltHoraDesde">A que hora?</label>
@@ -33,7 +44,9 @@
             </div>
             <div class="row justify-content-center">
               <div class="col-md-4 text-center">
-                <button type="submit" class="btn btn-primary btn-block">Guardar</button>
+                <button type="submit" class="btn btn-primary btn-block">
+                  Guardar
+                </button>
               </div>
             </div>
           </form>
